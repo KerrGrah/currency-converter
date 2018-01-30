2d780652d82cfd26bef3c172702cee1151f8eb56
Graham <grey.ham.acre@gmail.com>
2018-01-30 23:06:16 +0300

    added commas to saved sums

--- src/components/Saved.js
+++ src/components/Saved.js
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
-import arrowSVG from '../images/arrow-up.js'
+import arrowSVG from '../images/arrow-up.js';
+import {numberWithCommas} from '../util-functions';
 
 export default class Saved extends PureComponent {
   deleteItem(key) {
@@ -13,7 +14,7 @@ export default class Saved extends PureComponent {
     }
     const savedExchanges = data.map((item, index) => <li key={"saved-exchange-" + item.key}>
       <div>
-        <p>{item.from} {item.inputAmount}{arrowSVG('right')}{item.converted} {item.to}
+        <p>{item.from} {numberWithCommas(item.inputAmount)}{arrowSVG('right')}{numberWithCommas(item.converted)} {item.to}
         </p>
         <button className="delete-button" onClick={this.deleteItem.bind(this, item.key)}>X</button>
       </div>
